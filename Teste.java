0f3eeb8cbdaf23ccb0667a6e1466b753067abb58
AllissonFelipe <154332420+AllissonFelipe@users.noreply.github.com>
2024-01-08 16:53:48 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,11 @@
+/**
+ * Teste
+ */
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println(ImprimirTextoMaiusculo("Hello World " + (1+6 *7)));
+    }
+    public static String ImprimirTextoMaiusculo(String Texto) {
+        return Texto.toUpperCase();
+    }
+}
